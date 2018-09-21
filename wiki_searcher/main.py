068556a756d162e47c09d1a977ea2773398c3023
via96 <via96rus@gmail.com>
2018-09-21 15:01:27 +0500

    + голосовое управление

--- wiki_searcher/main.py
+++ wiki_searcher/main.py
@@ -1,5 +1,12 @@
 import wikipedia
 import subprocess
+import recognizer
+
+def get_word(voice_mod: bool):
+    if voice_mod:
+        return recognizer.listen()
+    else:
+        return input('Введите слово:')
 
 def read_text(text: str):
     print(text)
@@ -8,12 +15,21 @@ def read_text(text: str):
 
 if __name__ == '__main__':
     wikipedia.set_lang("ru")
+    sent_amount = 4
     while True:
-        word = input('Введите слово:')
+        word = ''
+        word = get_word(True)
+        if word == '': continue
+        if word == 'выход':
+            break
+        if word.__contains__('количество предложений'):
+            sent_amount = str(word.split(' ')[-1])
+            read_text('Количество выводимых предложений: ' + str(sent_amount))
+            continue
         try:
             if word == 'exit':
                 break
-            response_list = wikipedia.search(word, results=2)
+            response_list = wikipedia.search(word)
             if len(response_list) == 0:
                 read_text("Введенное слово неверно или определение не найдено.")
                 print('\n----------------------------------------------------------------------------\n')
@@ -23,8 +39,9 @@ if __name__ == '__main__':
             print('\n----------------------------------------------------------------------------\n')
             continue
         try:
-            text = wikipedia.summary(response_list[0], sentences=4)
+            text = wikipedia.summary(response_list[0], sentences=sent_amount)
         except wikipedia.exceptions.DisambiguationError:
-            text = wikipedia.summary(response_list[1], sentences=4)
+            text = wikipedia.summary(response_list[1], sentences=sent_amount)
         read_text(text)
         print('\n----------------------------------------------------------------------------\n')
+    read_text('До свидания.')
