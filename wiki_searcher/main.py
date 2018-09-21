6cb7f1f4015761ca06fbe1fcb5dbc4d77e86111e
via96 <via96rus@gmail.com>
2018-09-21 11:02:31 +0500

    + обработка многозначности(требуется модификация библиотеки, см. readme) + переход на Popen для вывода голоса

--- wiki_searcher/main.py
+++ wiki_searcher/main.py
@@ -1,6 +1,11 @@
 import wikipedia
 import subprocess
 
+def read_text(text: str):
+    print(text)
+    speaker = subprocess.Popen("espeak -vru -s 60 '" + text + "'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    speaker.communicate()
+
 if __name__ == '__main__':
     wikipedia.set_lang("ru")
     while True:
@@ -8,17 +13,18 @@ if __name__ == '__main__':
         try:
             if word == 'exit':
                 break
-            response_list = wikipedia.search(word)
-            amount = len(response_list)
-            if amount == 1:
-                text = wikipedia.summary(response_list[0], sentences=4)
-            else:
-                try:
-                    text = wikipedia.summary(response_list[0], sentences=4)
-                except:
-                    text = wikipedia.summary(response_list[1], sentences=4)
-            print(text)
-            # subprocess.call("espeak -vru -s 60 '" + text + "'", shell=True)
+            response_list = wikipedia.search(word, results=2)
+            if len(response_list) == 0:
+                read_text("Введенное слово неверно или определение не найдено.")
+                print('\n----------------------------------------------------------------------------\n')
+                continue
         except:
-            print("Введенное слово неверно или определение не найдено.")
+            read_text("Введенное слово неверно или определение не найдено.")
+            print('\n----------------------------------------------------------------------------\n')
+            continue
+        try:
+            text = wikipedia.summary(response_list[0], sentences=4)
+        except wikipedia.exceptions.DisambiguationError:
+            text = wikipedia.summary(response_list[1], sentences=4)
+        read_text(text)
         print('\n----------------------------------------------------------------------------\n')
